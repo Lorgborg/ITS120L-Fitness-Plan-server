13fc4a48525e8fd65892f347f60b5416983973ba
Lorgborg <66427174+Lorgborg@users.noreply.github.com>
2025-03-16 22:06:05 +0800

    Update routes.js

--- src/api/routes.js
+++ src/api/routes.js
@@ -7,19 +7,6 @@ router.get("/", (req, res) => {
     res.json({ message: "Hello from API!" });
 });
 
-async function connectToMongoDB() {
-    try {
-      // Connect to the MongoDB server
-      await client.connect();
-      console.log('Connected to MongoDB');
-  
-      // Return the connected client~
-      return client;
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      throw error; // Re-throw the error to handle it outside the function
-    }
-}
 
 router.post("/getUser", async (req, res) => {
     console.log(req.body)
