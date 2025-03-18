39da8cd0606c3a01af058c33195b2183505ccd2d
Lorgborg <66427174+Lorgborg@users.noreply.github.com>
2025-03-18 13:30:54 +0800

    added cookie parser

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const MongoDBStore = require('connect-mongodb-session')(session);
+const cookieParser = require('cookie-parser')
 require("dotenv").config();
 
 const app = express();
@@ -25,6 +26,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(express.json());
 app.use((req, res, next) => {
     console.log("Cookies:", req.cookies);
@@ -43,6 +45,7 @@ app.use(session({
 
 // Import API routes
 const apiRoutes = require("./api/routes");
+const cookieParser = require("cookie-parser");
 app.use("/api", apiRoutes);
 
 // Default route
