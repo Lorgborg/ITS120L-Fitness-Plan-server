10627cb45413024e68892d5342fad65c6fecdbbc
Lorgborg <66427174+Lorgborg@users.noreply.github.com>
2025-03-18 12:44:26 +0800

    Update routes.js

--- src/api/routes.js
+++ src/api/routes.js
@@ -82,8 +82,13 @@ router.post("/login", async (req, res) => {
     
 })
 
-router.post('/profile', (req, res) => {
-    if (!req.session.user) return res.status(401).json({ message: 'Not logged in' });
+router.post('/profile', async (req, res) => {
+    if (!req.session.user) {
+        // return res.status(401).json({ message: 'Not logged in' });
+        const collection = connection.db('MyFit').collection("users");
+        const user = await collection.findOne({email: req.body.email})
+        res.status(201).json({ message: 'Profile data', user: user });
+    }
  
     res.status(201).json({ message: 'Profile data', user: req.session.user });
  });
